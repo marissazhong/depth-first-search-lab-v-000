6ef621d6c683f8b3f761de253233d9274245ecdb
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 14:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 function depthFirstSearch(rootNode, vertices, edges) {
   let visited = [];
-  rootNode.discovered = "yes";
-  
+  let queue = [rootNode];
+  while (queue.length != 0) {
+    let currentNode = queue.pop();
+    if (!currentNode.discovered) {
+      rootNode.discovered = "yes";
+    }
+  }
 
   return visited;
 }
